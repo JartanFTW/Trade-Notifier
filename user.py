dde18bdee81e202c5d28840b7b1e7bef580a674f
jartan <jgogox@gmail.com>
2021-01-20 21:35:40 -0500

    Created User class with user-specific functions

---
+++ user.py
@@ -0,0 +1,193 @@
+import httpx
+import asyncio
+import logging
+from utilities import timestamp_print, UnknownResponse, InvalidCookie
+import sys
+
+
+class User():
+
+    @classmethod # Factory method to allow for async initialization function calls
+    async def create(cls, security_cookie):
+
+        self = User()
+
+        self.client = httpx.AsyncClient(cookies={})
+
+        self.client.cookies[".ROBLOSECURITY"] = security_cookie
+
+        await self.update_csrf()
+
+        return self
+
+
+
+    async def update_csrf(self):
+
+        while True:
+
+            logging.info("Updating user csrf token")
+
+            request = await self.client.post("https://auth.roblox.com/v1/logout")
+
+            try:
+
+                self.client.cookies["X-CSRF-TOKEN"] = request.headers["x-csrf-token"]
+
+                logging.debug(f"Updated user csrf token: {request.headers['x-csrf-token']}")
+
+                return
+            
+            except KeyError:
+
+                logging.warning(f"Failed to update user csrf token: {request.status_code}")
+
+                if request.status_code == 429:
+                    
+                    await asyncio.sleep(5)
+
+                    continue
+
+                elif request.status_code == 401:
+
+                    logging.critical("Cookie is invalid")
+                    
+                    raise InvalidCookie(request.status_code, request.url, response_text = request.text)
+            
+                else:
+
+                    logging.critical(f"Encountered an unknown response code while updating user csrf: {request.status_code}")
+
+                    raise UnknownResponse(request.status_code, request.url, response_text = request.text)
+
+
+
+    async def update_id(self):
+
+        attempt = 0
+
+        while True:
+
+            logging.info("Updating user id")
+
+            request = await self.client.get("https://www.roblox.com/game/GetCurrentUser.ashx")
+
+            if request.status_code == 200 and request.text != "null":
+
+                self.id = request.text
+
+                logging.debug(f"Updated user id: {request.text}")
+
+                return
+            
+            logging.warning(f"Failed to update user id: {request.status_code}")
+
+            if request.status_code == 429:
+
+                await asyncio.sleep(5)
+
+                continue
+
+            else:
+
+                attempt += 1
+                
+                if attempt > 2:
+                    
+                    logging.critical(f"Unable to update user id: {request.status_code}")
+
+                    raise UnknownResponse(request.status_code, request.url, response_text = request.text)
+
+                logging.error(f"Unable to update user id: {request.status_code}")
+                
+                await self.update_csrf()
+
+                continue
+
+
+
+    async def get_trade_status_data(self, tradeStatusType: str = "Inbound", limit: int = 10, sortOrder: str = "Asc"):
+
+        attempt = 0
+
+        while True:
+
+            logging.info("Grabbing user trade status data")
+
+            request = await self.client.get(f"https://trades.roblox.com/v1/trades/{tradeStatusType}?limit={limit}&sortOrder={sortOrder}")
+
+            if request.status_code == 200:
+
+                request_json = request.json()
+
+                logging.debug(f"Grabbed user trade status data: {request_json}")
+
+                return request_json
+
+            logging.warning(f"Failed to grab user trade status data: {request.status_code}")
+
+            if request.status_code == 429:
+
+                await asyncio.sleep(5)
+
+                continue
+
+            else:
+
+                attempt += 1
+
+                if attempt > 2:
+
+                    logging.critical(f"Unable to grab user trade status data: {request.status_code}")
+
+                    raise UnknownResponse(request.status_code, request.url, response_text = request.text)
+                
+                logging.error(f"Unable to grab user trade status data: {request.status_code}")
+
+                await self.update_csrf
+
+                continue
+
+
+
+    async def get_trade_data(self, trade_id):
+
+        attempt = 0
+
+        while True:
+
+            logging.info("Grabbing user trade data")
+
+            request = await self.client.get(f"https://trades.roblox.com/v1/trades/{trade_id}")
+
+            if request.status_code == 200:
+
+                request_json = request.json()
+
+                logging.debug(f"Grabbed user trade data: {request_json}")
+
+                return request_json
+
+            logging.warning(f"Failed to grab user trade data: {request.status_code}")
+
+            if request.status_code == 429:
+
+                await asyncio.sleep(5)
+
+                continue
+            
+            else:
+
+                attempt += 1
+
+                if attempt > 2:
+
+                    logging.critical(f"Unable to grab user trade data: {request.status_code}")
+
+                    raise UnknownResponse(request.status_code, request.url, response_text = request.text)
+                
+                logging.error(f"Unable to grab user trade data: {request.status_code}")
+
+                await self.update_csrf
+
+                continue
