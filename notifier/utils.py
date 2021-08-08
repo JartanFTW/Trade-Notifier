eaf15909eea8c4ed65fced2265aba44c88916b95
Jartan <jgogox@gmail.com>
2021-08-08 15:19:34 -0400

    Created utils.py

--- notifier/utils.py
+++ notifier/utils.py
@@ -12,6 +12,9 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from .errors import *
-from .roblox import *
-from .utils import *
+
+# Standard Library
+import asyncio
+import logging
+
+log = logging.getLogger(__name__)
