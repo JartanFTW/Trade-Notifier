e4e228799671ce894e8500304a381d70e6ca4a2d
Jartan <jgogox@gmail.com>
2021-08-08 15:29:19 -0400

    Created load_config, added typehinting, added imports

--- notifier/utils.py
+++ notifier/utils.py
@@ -15,16 +15,38 @@
 
 # Standard Library
 import asyncio
+from configparser import ConfigParser, SectionProxy
 import logging
+import os
 import time
 
 log = logging.getLogger(__name__)
 
 
-def format_print(text: str, log_level: int = None):
+def format_print(text: str, log_level: int = None) -> None:
     """Prints to console the provided string with a H:M:S | timestamp before it
     If log_level is provided it will also log the text without formatting, at the provided level."""
     formatted_text = time.strftime("%H:%M:%S | ", time.localtime()) + text
     print(formatted_text)
     if isinstance(log_level, int):
         log.log(log_level, text)
+
+
+def load_config(path: str) -> dict:
+    """Loads config from path provided and returns it formatted as a dict"""
+    parser = ConfigParser()
+    parser.read(path)
+
+    def wrap_to_dict(to_wrap):
+        wrapped = dict(to_wrap)
+        for key, value in wrapped.items():
+            if isinstance(value, ConfigParser) or isinstance(value, SectionProxy):
+                wrapped[key] = wrap_to_dict(value)
+            elif value.upper().strip() in ("TRUE", "FALSE"):
+                wrapped[key] = True if value.upper().strip() == "TRUE" else False
+            else:
+                try:
+                    wrapped[key] = int(value)
+                except ValueError:
+                    pass
+        return wrapped
